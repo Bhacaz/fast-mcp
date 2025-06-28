87750e14fbb9ee48fa6403a4d000b5503ce8832e
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-06-28 08:07:06 -0400

    pass block

--- lib/fast_mcp.rb
+++ lib/fast_mcp.rb
@@ -143,7 +143,7 @@ module FastMcp
   # @yield [server] A block to configure the server
   # @yieldparam server [FastMcp::Server] The server to configure
   # @return [#call] The Rack middleware
-  def self.mount_in_rails(app, options = {})
+  def self.mount_in_rails(app, options = {}, &block)
     # Default options
     name = options.delete(:name) || app.class.module_parent_name.underscore.dasherize
     version = options.delete(:version) || '1.0.0'
@@ -152,9 +152,9 @@ module FastMcp
 
     # Handle transport-specific options
     if transport_type == :legacy
-      setup_legacy_rails_transport(app, options.merge(name: name, version: version, logger: logger))
+      setup_legacy_rails_transport(app, options.merge(name: name, version: version, logger: logger), &block)
     else
-      setup_streamable_rails_transport(app, options.merge(name: name, version: version, logger: logger), transport_type)
+      setup_streamable_rails_transport(app, options.merge(name: name, version: version, logger: logger), transport_type, &block)
     end
   end
 
